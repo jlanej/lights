579bc2860adda904d45b1b9c6b1151f81f491f32
jlanej <jlanej@gmail.com>
2023-05-07 11:30:39 -0500

    test blending

--- blend.py
+++ blend.py
@@ -1,6 +1,7 @@
-import cv2
+import sys
 import numpy as np
-import matplotlib.image as mpimg
+import cv2
+import glob
 
 
 # https://stackoverflow.com/questions/57723968/blending-multiple-images-with-opencv
@@ -12,11 +13,27 @@ def blend(list_images): # Blend images equally.
 
     for img in list_images:
         output = output + img * equal_fraction
+        # output = cv2.addWeighted(output, 1, img, equal_fraction, 0)
 
     output = output.astype(np.uint8)
     return output
 
 
+def read_image_list(file_list_images):
+    image_list = []
+    for filename in file_list_images:
+        im = cv2.imread(filename)
+        image_list.append(im)
+
+    return image_list
+
+
 if __name__ == '__main__':
-    past_frame = None
-    print("Blending lapse")
+    dir_img = sys.argv[1]
+    dir_out_img = sys.argv[2]
+
+    # print("blending" +len(glob.glob(dir_img+'/*.jpg')))
+    images = read_image_list(glob.glob(dir_img+'/*.jpg'))
+    blended = blend(images)
+    cv2.imwrite(dir_out_img+"test.jpg", blended, [cv2.IMWRITE_JPEG_QUALITY, 95])
+
